8fb8fb87152e6e93d47f220a196140b944bc6bef
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-01-13 17:50:17 -0500

    Display image

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import screenImage from "../assets/images/background-image.png"
+import { StatusBar } from "expo-status-bar";
+
+const PlaceholderImage = require('../assets/images/background-image.png')
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
+      <View style={styles.imageContainer}>
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Image source= {PlaceholderImage} style={styles.image}/>
       </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,6 +22,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
+    backgroundColor: "#25292e",
   },
   main: {
     flex: 1,
@@ -26,9 +33,20 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 64,
     fontWeight: "bold",
+    color: "#fff",
+
   },
   subtitle: {
     fontSize: 36,
-    color: "#38434D",
+  color: "#fff",
   },
+  imageContainer: {
+    flex:1,
+    paddingTop:58,
+  },
+  image: {
+    width:320,
+    height:440,
+    borderRadius:18,
+  }
 });
